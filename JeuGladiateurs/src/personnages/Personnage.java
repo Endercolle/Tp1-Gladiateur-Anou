a1e3fcff6251e8d591f069880f3ae04e79ee0368
202133520 <202133520@PO-24846>
2024-09-13 13:22:23 -0400

    Test Patrice

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -138,7 +138,11 @@ public class Personnage {
     }
 
     public void setNewInitiativeRandom() {
-        // TODO : Modifier de façon aléatoire la valeur INI du personnage.
+       Random rand = new Random();
+       int minValue = 0;
+       int maxValue = 100 ;
+       int nombreAleatoire = rand.nextInt(maxValue - minValue) + minValue;
+       initiative = nombreAleatoire;
     }
     // </editor-fold>
 }

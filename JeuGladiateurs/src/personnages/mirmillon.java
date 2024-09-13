337e964acb348ecdcf41774186e601cc676f2d93
202133520 <202133520@PO-24846>
2024-09-13 14:03:49 -0400

    Test Patrice

--- JeuGladiateurs/src/personnages/mirmillon.java
+++ JeuGladiateurs/src/personnages/mirmillon.java
@@ -2,7 +2,21 @@
 package personnages;
 
 
-public class mirmillon 
+public class mirmillon extends Personnage
 {
+boolean doubleTour;
+    public mirmillon(String nom, int attaqueMax, int defense, int pvs, int ini) {
+        super(nom, attaqueMax, defense, pvs, ini);
+        initiative = 30;
+        doubleTour = true;
+    }
+
+    public boolean isDoubleTour() {
+        return doubleTour;
+    }
+
+    public void setDoubleTour(boolean doubleTour) {
+        this.doubleTour = doubleTour;
+    }
     
 }

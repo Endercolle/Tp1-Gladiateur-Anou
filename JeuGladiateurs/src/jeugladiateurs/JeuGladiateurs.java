fb36b175b080453ad3e391d64d4bac59133dd711
202133520 <202133520@PO-24846>
2024-09-13 14:40:11 -0400

    Test Patrice

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -16,8 +16,8 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
     CompteurDeTour tour = new CompteurDeTour();
     AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Mirmillon("Bob le Malchanceux",15,15,70,15);
-    Personnage Igor = new Retiaire("Igor l'empaleur",25,5,100,30);
+    Mirmillon Bob = new Mirmillon("Bob le Malchanceux",15,15,70,15);
+    Retiaire Igor = new Retiaire("Igor l'empaleur",25,5,100,30);
     // </editor-fold>
 
     // **************************************************************************
@@ -35,13 +35,26 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
         do 
         {
-           
+          
             tour.afficheTour();
             for (int i = 0; i < 100; i++) 
             {
                 if (Bob.getInitiative() == i && Bob.getPointsDeVie()> 0 ) 
                 {
                     Bob.frapperPersonnage(Igor);
+                    if (Bob.isDoubleTour() && Igor.getPointsDeVie() > 0) 
+                    {
+                        System.out.println("Bob attaque une deuxième fois");
+                        System.out.println("");
+                        Bob.frapperPersonnage(Igor);
+                        Bob.setDoubleTour(false);
+                    }
+                    else if(Bob.isDoubleTour() && Igor.getPointsDeVie() == 0)
+                    {
+                        System.out.println("Bob décapite Igor");
+                        System.out.println("");
+                        break;
+                    }
                     System.out.println("");
                     Igor.frapperPersonnage(Bob);
                     break;
@@ -61,6 +74,7 @@ public class JeuGladiateurs {
             Igor.setNewInitiativeRandom();
             tour.augmenteTour();
             affichage.afficherSeparateurDeTour();
+             Bob.setDoubleTour(true);
             
         } while (Bob.getPointsDeVie() > 0 && Igor.getPointsDeVie() > 0);
         affichage.afficheVictoire(Bob, Igor);

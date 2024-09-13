bcff5f2ae551a8bf77c1d9baba66ef7e086b09c0
202133520 <202133520@PO-24846>
2024-09-13 14:39:55 -0400

    Test Patrice

--- JeuGladiateurs/src/personnages/Mirmillon.java
+++ JeuGladiateurs/src/personnages/Mirmillon.java
@@ -1,13 +1,11 @@
-
 package personnages;
 
 import java.util.Random;
 
+public class Mirmillon extends Personnage {
 
+    private boolean doubleTour;
 
-public class Mirmillon extends Personnage
-{
-private boolean doubleTour;
     public Mirmillon(String nom, int attaqueMax, int defense, int pvs, int ini) {
         super(nom, attaqueMax, defense, pvs, ini);
         doubleTour = true;
@@ -20,12 +18,13 @@ private boolean doubleTour;
     public void setDoubleTour(boolean doubleTour) {
         this.doubleTour = doubleTour;
     }
-@Override
+
+    @Override
     public void setNewInitiativeRandom() {
-       Random rand = new Random();
-       int minValue = 0;
-       int maxValue = 30;
-       int nombreAleatoire = rand.nextInt(maxValue - minValue) + minValue;
-       initiative = nombreAleatoire;
+        Random rand = new Random();
+        int minValue = 0;
+        int maxValue = 30;
+        int nombreAleatoire = rand.nextInt(maxValue - minValue) + minValue;
+        initiative = nombreAleatoire;
     }
 }
